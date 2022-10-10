2ff8abbf591714d29c68400ce643f7d884320fb1
Lindsey Archipley <lindsey.archipley@gmail.com>
2022-10-10 19:56:29 -0400

    Add new button & event listener for grid size

--- javascript.js
+++ javascript.js
@@ -7,9 +7,6 @@ for (let i = 0; i < 256; i++) {
   div.classList.add('square');
   container.appendChild(div);
   };
-
-
-
 //create 256 of the same divs (need 16x16 block)
 
 const divs = document.querySelectorAll(".square");
@@ -18,3 +15,22 @@ divs.forEach((divs) => {
         divs.classList.add('colorChange');
     })
 });
+//adds new class to each div when it is mouse-overed; this new class changes the color
+
+
+//when btn is clicked
+//prompt user for number
+//store entered # in variable
+//set maximum # to 100
+
+let userInput;
+
+const newGridBtn = document.querySelector('#newGridBtn');
+newGridBtn.addEventListener("click", () => {
+    userInput = prompt("How many squares would you like each side of the grid to have?");
+    while (userInput > 100) {
+        userInput = prompt("Please choose a number between 1 and 100");
+    }
+        console.log(userInput);
+    });
+
